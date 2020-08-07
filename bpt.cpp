75b906b61eda8f04fbf6c49a549f28c66e264f4f
tpy <tpy@MBA.local>
2020-08-07 22:41:41 +0800

    garbage commit

--- bpt.cpp
+++ bpt.cpp
@@ -21,73 +21,18 @@ struct Block
 	Chunk chunk;//include hash value and PBA
 	struct Block* child[MAX_DEGREE];
 	struct Block* parent;
+	struct Block* next;
 	Block(){
 		size=0;
 		is_leaf=false;
 		parent=nullptr;
+		next=nullptr;
 		for (int i = 0; i < MAX_DEGREE; ++i){
 			child[i]=nullptr;
 		}
 	}
 };
 Block *root=nullptr; //the B+ tree, declare as global variable
-Block *search(uint64_t key){
-	Block *p=root;
-	if(root==nullptr){
-		cout <<"tree is empty\n";
-		return nullptr;
-	}
-	cout <<"path: ";
-	bool fit=false;
-	while(!p->is_leaf){
-		for (int i=0;i<MAX_DEGREE-1;i++){
-			cout <<p->chunk.key[i]<<">";
-			if(key<p->chunk.key[i] || key==p->chunk.key[i] ){
-				fit=true;
-					p=p->child[i];
-				break;
-			}
-		}
-		if(!fit)
-			p=p->child[MAX_DEGREE-1];
-		//not in the tree -> return a nullptr
-		if(p==nullptr)
-			break;
-	}
-			return p;
-	//return nullptr;
-}
-void insert(uint64_t key,int val){
-	//case:size of tree is 0 ->build a new block
-	
-	if (root==nullptr){
-		//in this case, root is the leaf node
-		root=new Block();
-		root->size++;
-		root->is_leaf=true;
-		root->chunk.key[0]=key;
-		root->chunk.val=new int[MAX_DEGREE-1]();
-		root->chunk.val[0]=val;
-		return;
-	}
-	//case:block is not full
-	if(root->size!=MAX_DEGREE){
-
-	}
-	//case:block is full
-	else{
-
-	}
-	root->size++;
-
-
-
-
-
-
-
-
-}
 void printBlock(Block *p){
 	if(p==nullptr)
 		return;
@@ -109,8 +54,12 @@ void printBlock(Block *p){
 	cout <<"| ";
 }
 void printTree(queue<Block *> q,int level){
+	if(q.front()==nullptr){
+		cout << "is empty\n";
+		return;
+	}
 	queue<Block *>tmp;
-	cout <<"\n*****level : "<<level<<"*****\n | ";
+	cout <<"\n*****level: "<<level<<"size: "<<q.front()->size<<"*****\n | ";
 	while(q.size()!=0){
 		printBlock(q.front());
 		for(int i=0;i<MAX_DEGREE;i++){
@@ -123,6 +72,138 @@ void printTree(queue<Block *> q,int level){
 		printTree(tmp,level+1);
 }
 
+Block *search(uint64_t key){
+	Block *p=root;
+	if(root==nullptr){
+		cout <<"tree is empty\n";
+		return nullptr;
+	}
+	//cout <<"path: ";
+	bool fit=false;
+	while(!p->is_leaf){
+		for (int i=0;i<MAX_DEGREE-1;i++){
+			//cout <<p->chunk.key[i]<<">";
+			if(key<p->chunk.key[i] || key==p->chunk.key[i] ){
+				fit=true;
+					p=p->child[i];
+				break;
+			}
+		}
+		if(!fit)
+			p=p->child[MAX_DEGREE-1];
+		//not in the tree -> return a parent (is_leaf==0)
+		if(p==nullptr){
+			p=p->parent;
+			break;
+		}
+	}
+			return p;
+	//return nullptr;
+}
+Block* adjust(Block * node,uint64_t key){
+	int split=(MAX_DEGREE-1)/2;
+	Block * current=node;
+	Block * prev=node->parent;
+	uint64_t oldkey[MAX_DEGREE];
+	uint64_t oldval[MAX_DEGREE-1];
+	uint64_t target;
+	Block * oldBlock;
+	for (int j=0,i=0;i<MAX_DEGREE-1;i++){
+		if((key<current->chunk.key[i] ||key==current->chunk.key[i]) && j==0){
+			oldkey[i+j]=key;
+			j++;
+		}
+		oldval[i]=current->chunk.val[i];
+		oldkey[i+j]=current->chunk.key[i];
+	}
+	for (int i=0;i<MAX_DEGREE;i++){
+		cout <<oldkey[i]<<" ";
+	}
+	target=oldkey[split];
+	while(current->size==MAX_DEGREE){
+		if(current->is_leaf){
+			prev->size++;
+			if(prev->size<MAX_DEGREE){
+				prev.child[prev->size]=new Block();
+				prev->child[prev->size-1]=prev->child[prev->size]->size=0;
+				prev->chunk[prev->size]->parent=prev;
+				int oldindex=0,newindex=0;
+				for (int i=0;i<MAX_DEGREE;i++){
+					if(oldval[i]<=target){
+						prev->child[prev->size]->size++;
+						prev->child[prev->size]->chunk.key[newindex]=oldval[i];
+					}
+					else{
+						prev->child[prev->size-1]->size++;
+						prev->child[prev->size-1]->chunk->key[oldindex]=oldval[i];
+					}
+				}
+			}
+			else{
+
+			}
+		}
+		else{
+
+		}
+		current=current->parent;
+		prev=prev->parent;
+	}
+}
+void insert(uint64_t key,int val){
+	//case:size of tree is 0 ->build a new block
+	
+	if (root==nullptr){
+		//in this case, root is the leaf node
+		root=new Block();
+		root->size++;
+		root->is_leaf=true;
+		root->chunk.key[0]=key;
+		root->chunk.val=new int[MAX_DEGREE-1]();
+		root->chunk.val[0]=val;
+		return;
+	}
+	Block *position=search(key);
+	//whether the position is leaf
+	if(!position->is_leaf){
+		int i=0;
+		for (i=0;i<MAX_DEGREE-1;i++){
+			if(key<position->chunk.key[i] || key==position->chunk.key[i])
+				break;
+		}
+		if(position->child[i]==nullptr){
+			position->child[i]=new Block();
+			if(i!=0)
+				position->child[i-1]->next=position->child[i];
+			position->child[i]->parent=position->child[i];
+		}
+		position=position->child[i];
+	}
+	//whether the leaf is not full
+	if(position->size!=MAX_DEGREE-1){
+		int i=0;
+		for ( i=0;i<MAX_DEGREE-1;i++)
+			if(key<position->chunk.key[i] || key==position->chunk.key[i] )
+				break;
+		for (int j=i;j<MAX_DEGREE-2;j++){
+			position->chunk.key[j+1]=position->chunk.key[j];
+			position->chunk.val[j+1]=position->chunk.val[j];
+		}
+		position->chunk.key[i]=key;
+		position->chunk.val[i]=val;
+		position->size++;
+	}
+	//whether the leaf is full -> adjust
+	else{
+		position->size++;
+		Block *newBlock=adjust(position,key);
+	}
+	queue<Block *> q;
+	q.push(root);
+	printTree(q,1);
+	cout <<endl;
+}
+
 int main(){
 	Block bb;
 	cout <<"size of block is "<<sizeof(bb )<<endl;
@@ -130,8 +211,16 @@ int main(){
 	cout <<"size of block* is "<<sizeof(bb.parent )<<endl;
 	cout <<"size of block* is "<<sizeof(bb.child )<<endl;
 	cout <<"size of bool is "<<sizeof(bb.is_leaf )<<endl;
+	queue<Block *>q;
+	q.push(root);
+	printTree(q,1);
+	insert(1,1);
+	insert(2,2);
+	insert(3,2);
+	insert(2,2);
 
-	/*build a tree artificially*/
+
+	/*build a tree artificially
 	root=new Block();
 	root->is_leaf=false;
 	root->chunk.key[0]=12;
@@ -160,12 +249,16 @@ int main(){
 	root->child[0]->child[0]->chunk.key[0]=9;
 	root->child[0]->child[0]->chunk.val[1]=8;
 	root->child[0]->child[0]->chunk.key[1]=8;
-	
+	*/
 
-	/*queue<Block *>q;
+	/*
+	queue<Block *>q;
 	q.push(root);
-	printTree(q,1);*/
+	printTree(q,1);
+	*/
+
 	//insert(1,1);
+	/*
 	Block *find=search(5);
 	for (int i=0;i<MAX_DEGREE-1;i++){
 		if(find!=nullptr){
@@ -174,6 +267,7 @@ int main(){
 			cout <<find->chunk.key[i]<<" "<<find->chunk.val[i]<<" ";
 		}
 	}
+	*/
 }
 
 
